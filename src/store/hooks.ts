3de0c8dd40c8e393dbc23a91ed6183e61d165181
ayoub-oumha <ayyouboumha@gmail.com>
2026-02-04 11:31:24 +0100

    first commit

--- src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './index';
-
-// Hooks typés pour utiliser dans toute l'application
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
